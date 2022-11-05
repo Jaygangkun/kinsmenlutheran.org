cb49e3432c67529e7e2e191338a3ce3fcf715b8c
JayGangkun <jaygangkun@hotmail.com>
2022-11-04 22:12:57 -0400

    theme download

--- wp-content/themes/dunamis/page-podcast.php
+++ wp-content/themes/dunamis/page-podcast.php
@@ -1,96 +1,96 @@
-<?php
-/**
- * The template for displaying member pages.
- *
- *
- Template Name: Itunes feed
- */
- 
-header("Content-Type: application/rss+xml; charset=UTF-8");
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-
- $wl = get_locale();  
- 
- $posts_per_rss = get_option('posts_per_rss');
- 
-   $args = array(
-	'numberposts' => $posts_per_rss,
-	'post_type' => 'mediacast',	
-	'meta_query' => array(
-        'relation' => 'OR',
-        array(
-            'key'     => 'cro_mediamp3',
-            'value'   => '',
-            'compare' => '!=',
-        ),
-        array(
-            'key'     => 'cro_mp3alt',
-            'value'   => '',          
-            'compare' => '!=',
-        ),
-    ),	
-   );
-   
-   $feeds = new WP_Query($args);
-
-?>
-
-<?php while ( have_posts() ) : the_post();
-	$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-endwhile; ?>
-<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
-
-	<channel>
-
-		<title><?php echo get_bloginfo('name'); ?></title>
-
-		<link><?php echo home_url(); ?></link>	
-
-		<language><?php echo $wl; ?></language>
-
-		<copyright>&#x2117; &amp; &#xA9; <?php echo date("Y"); ?> <?php echo htmlspecialchars((get_bloginfo('name'))); ?></copyright>
-
-		<itunes:subtitle><?php echo htmlspecialchars((get_bloginfo('description'))); ?></itunes:subtitle>
-
-		<itunes:author><?php echo htmlspecialchars((get_bloginfo('name'))); ?></itunes:author>		
-	
-		<itunes:owner>
-
-			<itunes:name><?php echo htmlspecialchars((get_bloginfo('name'))); ?></itunes:name>
-
-			<itunes:email><?php echo get_option('admin_email'); ?></itunes:email>
-
-		</itunes:owner>	
-
-		<itunes:image href="<?php echo $image[0]; ?>" /> 		
-		
-		<itunes:category></itunes:category> 
-
-	<?php
-
-	while ( $feeds->have_posts() ) : $feeds->the_post();
-
-		if ( get_post_meta(get_the_ID(), 'cro_mp3alt', true) != ''  ){ 
-		
-			$aud_url = get_post_meta(get_the_ID(),'cro_mp3alt', true); 
-			
-		}else{
-		
-			$aud_url = get_post_meta(get_the_ID(),'cro_mediamp3', true);
-		}
-	?>
-
-		<item>
-			<title><?php the_title(); ?></title>
-			<itunes:author><?php echo htmlspecialchars((get_bloginfo('name'))); ?></itunes:author>
-			<itunes:summary><?php echo  htmlspecialchars(strip_tags(get_the_excerpt())); ?></itunes:summary> 
-			<enclosure url="<?php echo $aud_url; ?>"  type="audio/mpeg" /> 
-			<guid><?php echo $aud_url; ?></guid>
-			<pubDate><?php echo get_the_date('D, d M Y H:i:s T', '', ''); ?></pubDate>			
-		</item>			
-
-		<?php endwhile; ?>
-
-	</channel>
-
+<?php
+/**
+ * The template for displaying member pages.
+ *
+ *
+ Template Name: Itunes feed
+ */
+ 
+header("Content-Type: application/rss+xml; charset=UTF-8");
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+ $wl = get_locale();  
+ 
+ $posts_per_rss = get_option('posts_per_rss');
+ 
+   $args = array(
+	'numberposts' => $posts_per_rss,
+	'post_type' => 'mediacast',	
+	'meta_query' => array(
+        'relation' => 'OR',
+        array(
+            'key'     => 'cro_mediamp3',
+            'value'   => '',
+            'compare' => '!=',
+        ),
+        array(
+            'key'     => 'cro_mp3alt',
+            'value'   => '',          
+            'compare' => '!=',
+        ),
+    ),	
+   );
+   
+   $feeds = new WP_Query($args);
+
+?>
+
+<?php while ( have_posts() ) : the_post();
+	$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+endwhile; ?>
+<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+
+	<channel>
+
+		<title><?php echo get_bloginfo('name'); ?></title>
+
+		<link><?php echo home_url(); ?></link>	
+
+		<language><?php echo $wl; ?></language>
+
+		<copyright>&#x2117; &amp; &#xA9; <?php echo date("Y"); ?> <?php echo htmlspecialchars((get_bloginfo('name'))); ?></copyright>
+
+		<itunes:subtitle><?php echo htmlspecialchars((get_bloginfo('description'))); ?></itunes:subtitle>
+
+		<itunes:author><?php echo htmlspecialchars((get_bloginfo('name'))); ?></itunes:author>		
+	
+		<itunes:owner>
+
+			<itunes:name><?php echo htmlspecialchars((get_bloginfo('name'))); ?></itunes:name>
+
+			<itunes:email><?php echo get_option('admin_email'); ?></itunes:email>
+
+		</itunes:owner>	
+
+		<itunes:image href="<?php echo $image[0]; ?>" /> 		
+		
+		<itunes:category></itunes:category> 
+
+	<?php
+
+	while ( $feeds->have_posts() ) : $feeds->the_post();
+
+		if ( get_post_meta(get_the_ID(), 'cro_mp3alt', true) != ''  ){ 
+		
+			$aud_url = get_post_meta(get_the_ID(),'cro_mp3alt', true); 
+			
+		}else{
+		
+			$aud_url = get_post_meta(get_the_ID(),'cro_mediamp3', true);
+		}
+	?>
+
+		<item>
+			<title><?php the_title(); ?></title>
+			<itunes:author><?php echo htmlspecialchars((get_bloginfo('name'))); ?></itunes:author>
+			<itunes:summary><?php echo  htmlspecialchars(strip_tags(get_the_excerpt())); ?></itunes:summary> 
+			<enclosure url="<?php echo $aud_url; ?>"  type="audio/mpeg" /> 
+			<guid><?php echo $aud_url; ?></guid>
+			<pubDate><?php echo get_the_date('D, d M Y H:i:s T', '', ''); ?></pubDate>			
+		</item>			
+
+		<?php endwhile; ?>
+
+	</channel>
+
 </rss>
--- wp-content/themes/dunamis/sidebar.php
+++ wp-content/themes/dunamis/sidebar.php
@@ -1,43 +1,43 @@
-<?php
-// File Security Check
-if ( ! function_exists( 'wp' ) && ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
-    die ( 'You do not have sufficient permissions to access this page!' );
-}
-?><?php
-/**
- * The main sidebar file
- *
- *
- * @package Croma.framework
- * @subpackage Croma.framework
- * @since 1.0
- */
-
-$sidebarname = '0';
-
-$curquer 		= $wp_query->get_queried_object();
-
-if ( is_page() || is_single())  {  
-
-	$curquer 		= $wp_query->get_queried_object();
-
-	$sidebarbox 	= (get_post_meta($curquer->ID, 'cromabox_sbbar', true) == '')? 0 : get_post_meta($curquer->ID, 'cromabox_sbbar', true);	
-	$sidebarname 	= (get_post_meta($curquer->ID, 'cromabox_sidebarnames', true) == '')? 0 : get_post_meta($curquer->ID, 'cromabox_sidebarnames', true);	
-} elseif (is_category()) {
-	$cat_id         = get_query_var('cat');
-	$cat_vars         = get_option('taxonomy_' . $cat_id);
-	if ($cat_vars != '') {$sidebarname = $cat_vars['cro_cat_sbar'];} 	
-} 
-
-$sidebarname = ($sidebarname  == '0' || $sidebarname  == 'cro_sidebarmain')? 'cro_sidebarmain' :  'cro_' . $sidebarname ;
-
-
-?>
-
-<?php if ( is_active_sidebar($sidebarname) ) : ?>
-<aside class="cro_bodysidebar">
-	<ul class="mainwidget">
-		<?php dynamic_sidebar($sidebarname); ?>
-	</ul><!-- #secondary -->
-</aside>
+<?php
+// File Security Check
+if ( ! function_exists( 'wp' ) && ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    die ( 'You do not have sufficient permissions to access this page!' );
+}
+?><?php
+/**
+ * The main sidebar file
+ *
+ *
+ * @package Croma.framework
+ * @subpackage Croma.framework
+ * @since 1.0
+ */
+
+$sidebarname = '0';
+
+$curquer 		= $wp_query->get_queried_object();
+
+if ( is_page() || is_single())  {  
+
+	$curquer 		= $wp_query->get_queried_object();
+
+	$sidebarbox 	= (get_post_meta($curquer->ID, 'cromabox_sbbar', true) == '')? 0 : get_post_meta($curquer->ID, 'cromabox_sbbar', true);	
+	$sidebarname 	= (get_post_meta($curquer->ID, 'cromabox_sidebarnames', true) == '')? 0 : get_post_meta($curquer->ID, 'cromabox_sidebarnames', true);	
+} elseif (is_category()) {
+	$cat_id         = get_query_var('cat');
+	$cat_vars         = get_option('taxonomy_' . $cat_id);
+	if ($cat_vars != '') {$sidebarname = $cat_vars['cro_cat_sbar'];} 	
+} 
+
+$sidebarname = ($sidebarname  == '0' || $sidebarname  == 'cro_sidebarmain')? 'cro_sidebarmain' :  'cro_' . $sidebarname ;
+
+
+?>
+
+<?php if ( is_active_sidebar($sidebarname) ) : ?>
+<aside class="cro_bodysidebar">
+	<ul class="mainwidget">
+		<?php dynamic_sidebar($sidebarname); ?>
+	</ul><!-- #secondary -->
+</aside>
 <?php endif; ?>
